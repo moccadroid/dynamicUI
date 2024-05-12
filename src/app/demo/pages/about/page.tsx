d0f238418fb228e5a65d5ec1587e9a174043dc3c
moccadroid <manxx.acc@gmail.com>
2024-05-13 00:44:40 +0200

    some minor fixes

--- src/app/demo/pages/about/page.tsx
+++ src/app/demo/pages/about/page.tsx
@@ -5,7 +5,7 @@ import landingpageLayout from '../../layouts/landingpage.layout.json';
 import landingpageData from '../../data/landingpage.data.json';
 import type { LayoutConfig } from '@/dynamicUI/components/ComponentConfig';
 
-export default function HomePage() {
+export default function LandingPage() {
 
   return (
 
--- src/app/demo/pages/documentation/page.tsx
+++ src/app/demo/pages/documentation/page.tsx
@@ -6,14 +6,15 @@ import documentationData from '../../data/documentation.data.json';
 import type { LayoutConfig } from '@/dynamicUI/components/ComponentConfig';
 import { Stack } from '@chakra-ui/react';
 
-export default function HomePage() {
+export default function DocumentationPage() {
 
 
   return (
-    <Stack spacing={5}>
+    <Stack spacing={8}>
       { Object.entries(documentationData).map(([key, value]) => {
-        // @ts-ignore
-        return <Section layout={documentationLayout[key] as LayoutConfig} data={value} />;
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        return <Section key={key} layout={documentationLayout[key] as LayoutConfig} data={value} />;
       })}
 
     </Stack>
